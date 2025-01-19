4fae9bceb9cc7ffae82744f603000e60cbe29797
Oniani Gakha <97256548+OnianiGakha@users.noreply.github.com>
2025-01-19 23:22:56 +0100

    Update ext.py

--- TuneIn-main/ext.py
+++ TuneIn-main/ext.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin
 
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__, static_folder="app/static")
 app.config['SECRET_KEY'] = '90jrtyd90j87e9y7e4539w8y'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
