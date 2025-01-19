dbd88587e8c20ba1595680fa119ae4f8bfccfae5
Oniani Gakha <97256548+OnianiGakha@users.noreply.github.com>
2025-01-19 23:13:06 +0100

    Add files via upload

---
+++ TuneIn-main/ext.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin
+
+
+app = Flask(__name__, static_folder="static")
+app.config['SECRET_KEY'] = '90jrtyd90j87e9y7e4539w8y'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+import routes
+app.register_blueprint(routes.bp)
+
+import models
